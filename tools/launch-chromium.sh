89c59f12fc7bceefc66432fed9898171a7d3d855
akrahl <alexander.krahl@stud.htwk-leipzig.de>
2020-08-20 13:06:51 +0200

    add more scripts

---
+++ tools/launch-chromium.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+chromium-browser --remote-debugging-port=9222 --enable-quic --quic-version=h3-29
--- tools/make-scripts-executable.sh
+++ tools/make-scripts-executable.sh
@@ -3,4 +3,6 @@
 chmod +x ./build-images.sh
 chmod +x ../nginx-docker/startup.sh
 chmod +x ../docker/configure.sh
-chmod +x ./loop-requests.sh
+chmod +x ./loop-request.sh
+chmod +x ./loop-request-out.sh
+chmod +x ./launch-chromium.sh 
