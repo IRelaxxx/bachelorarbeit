a9da1b4655515f9f44be854f6ae9ce6c6e69e641
akrahl <alexander.krahl@stud.htwk-leipzig.de>
2020-08-23 11:57:50 +0200

    disable cache

--- tools/launch-chromium.sh
+++ tools/launch-chromium.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-chromium-browser --remote-debugging-port=9222 --enable-quic --quic-version=h3-29
+/opt/google/chrome-unstable/chrome --remote-debugging-port=9222 --enable-quic --quic-version=h3-29 --disable-application-cache --media-cache-size=1 --disk-cache-size=1
