9d0f82e5a2a1542abaa78885a9739107ff4a52a3
Alexander Krahl <alexanderkrahl@hotmail.de>
2020-09-29 13:34:06 +0200

    auto call configure, allow configure to emulate loss and limit latency at the same time

--- docker/configure.sh
+++ docker/configure.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ -n "${LOSS}" ]]; then
-  tc qdisc add dev eth0 root netem loss random $LOSS
+  tc qdisc add dev eth0 root handle 1: netem loss random $LOSS
 else
   echo "LOSS not set"
 fi
@@ -9,7 +9,11 @@ fi
 if [[ -n "${RATE}" ]]; then
   if [[ -n "${BURST}" ]]; then
     if [[ -n "${LATENCY}" ]]; then
-      tc qdisc add dev eth0 root tbf rate $RATE latency $LATENCY burst $BURST
+      if [[ -n "${LOSS}" ]]; then
+        tc qdisc add dev eth0 parent 1:1 handle 10: tbf rate $RATE latency $LATENCY burst $BURST
+      else
+        tc qdisc add dev eth0 root tbf rate $RATE latency $LATENCY burst $BURST
+      fi
     else
       echo "LATENCY not set"
     fi
--- nginx-docker/startup.sh
+++ nginx-docker/startup.sh
@@ -6,4 +6,8 @@ sed -i "s/DOMAIN/"$DOMAINNAME"/g" ../conf/nginx.conf
 
 cat ../conf/nginx.conf
 
+if test -f "/root/configure.sh"; then
+    /root/configure.sh
+fi
+
 ./nginx -g 'daemon off;'
